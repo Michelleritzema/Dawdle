122ff34a8b31118a27930305376eb0c3b87f3480
Michelleritzema <michelleritzema@hotmail.com>
2014-05-22 16:51:07 +0200

    Verbetering code en nieuwe achtergrond + naamstijl

--- Dawdle/src/com/example/dawdle/SplashScreen.java
+++ Dawdle/src/com/example/dawdle/SplashScreen.java
@@ -1,12 +1,12 @@
 package com.example.dawdle;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class SplashScreen extends ActionBarActivity {
+public class SplashScreen extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
